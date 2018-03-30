ed8b865a95e1aef87b6d6684fe73985953c14b6f
gaoyanru <962290336@qq.com>
2018-03-30 18:49:26 +0800

    [feat] 错误 related to issue #9

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'normalize.css/normalize.css'
-import 'element-ui/lib/theme-default/index.css'
+import 'element-ui/lib/theme-chalk/index.css'
 import '../static/main.css'
 import Vue from 'vue'
 import App from './App'
